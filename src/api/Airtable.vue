7eaf4880f287c82244296e693b28ab802735de4a
Powarox219 <moriniere.robin1877@gmail.com>
2022-01-13 19:31:10 +0100

    Update Get Request

--- src/api/Airtable.vue
+++ src/api/Airtable.vue
@@ -23,11 +23,20 @@
         mounted() {
             const base = new Airtable({apiKey: 'keyninpGxob6oPycb'}).base('appuxrEpFcUYHpYGu');
             base('Wallet').select({
-                maxRecords: 3,
+                // maxRecords: 3,
                 view: "Grid view"
             }).eachPage(response => {
                 for(let res in response) {
-                    this.data[res] = response[res].fields;
+                    this.data[res] = {
+                        'Name': response[res].fields['Name'],
+                        'Amounts': response[res].fields['Amounts'],
+                        'Coins': response[res].fields['Coins'],
+                        'MarketPrice': response[res].fields['Market Price'],
+                        'MarketValue': response[res].fields['Market Value'],
+                        'Profits$': response[res].fields['Profits $'],
+                        'Profits%': response[res].fields['Profits %'],
+                        'App': response[res].fields['App'][0],
+                    }
                 }
                 this.updateStore();
             }, function done(err) {
--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,6 +1,34 @@
 <template lang="html">
     <div id="overview">
-        wallet
+        <button @click="findWallet()">load</button>
+
+        <section class="views">
+            <h2>Mes Actifs</h2>
+            <div class="filter">
+                <h3>Name</h3>
+                <h3>Amounts</h3>
+                <h3>Coins</h3>
+                <h3>Current Price</h3>
+                <h3>Market Value</h3>
+                <h3>Profits $</h3>
+                <h3>Profits %</h3>
+                <h3>App</h3>
+            </div>
+            <div class="rows"  v-for="row in this.data" v-bind:key="row.Name">
+                <h3>{{ row.Name }}</h3>
+                <h3>{{ row.Amounts }}</h3>
+                <h3>{{ row.Coins }}</h3>
+                <h3>{{ row.CurrentPrice }}</h3>
+                <h3>{{ row.MarketValue }}</h3>
+                <h3>{{ row.Profits.toLocaleString('fr-FR') }}</h3>
+                <h3>{{ row.ROI }}</h3>
+                <h3>{{ row.Apps }}</h3>
+            </div>
+        </section>
+
+        <section class="">
+
+        </section>
     </div>
 </template>
 
@@ -12,8 +40,7 @@
         data() {
             return {
                 data: {},
-                loading: true,
-                errored: false,
+                succes: false,
             }
         },
         methods: {
@@ -23,12 +50,40 @@
             ...mapGetters([
                 'getData', 'getFeedback', 'getMessage',
             ]),
+
+            findWallet(){
+                this.data = this.getData();
+                console.log(this.data);
+            }
         },
+        computed: {
+        }
     }
 </script>
 
 <style lang="css" scoped>
     #overview {
         padding: 30px;
+        display: grid;
+        grid-template-columns: 1fr;
+    }
+
+    .views {
+        display: grid;
+        grid-template-columns: 1fr;
+    }
+
+    .views .filter {
+        display: grid;
+        grid-template-columns: repeat(8, 1fr);
+        align-items: center;
+        justify-items: center;
+    }
+
+    .views .rows {
+        display: grid;
+        grid-template-columns: repeat(8, 1fr);
+        align-items: center;
+        justify-items: center;
     }
 </style>
