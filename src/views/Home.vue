f44e4c074907f84dc7f1990ba5f4cf610717e7de
Powarox219 <moriniere.robin1877@gmail.com>
2022-01-13 15:15:57 +0100

    Add 2 views

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+    <div id="home">
+        <h1>404 Page Not Found</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+    }
+</script>
+
+<style lang="css" scoped>
+    #home {
+
+    }
+</style>
