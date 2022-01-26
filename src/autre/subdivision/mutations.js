c3ba3518687e0e2e5121b0ca6fae814c8463fd6c
Powarox219 <moriniere.robin1877@gmail.com>
2022-01-26 19:16:06 +0100

    Update store mutations

--- src/autre/subdivision/mutations.js
+++ src/autre/subdivision/mutations.js
@@ -1,4 +1,17 @@
 
 export function UPDATEDATA(state, data) {
     state.data = data;
+    state.loadData = true;
+}
+
+export function UPDATEFIELD(state, info) {
+    state.data[info.id].Amounts += info['amounts'];
+    state.data[info.id].Coins += info['quantity'];
+}
+
+export function UPDATEFIELD2(state, info) {
+    state.data[info.id].Amounts -= info['amounts'];
+    state.data[info.id].Coins -= info['quantity'];
+    state.data[info.stableID].Amounts += info['amounts'];
+    state.data[info.stableID].Coins += info['quantity'];
 }
