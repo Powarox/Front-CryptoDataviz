dcbce251e9a91454e4ae312016d8215cf758fd57
Powarox219 <moriniere.robin1877@gmail.com>
2022-01-27 21:34:02 +0100

    Update store

--- src/autre/subdivision/getters.js
+++ src/autre/subdivision/getters.js
@@ -3,6 +3,10 @@ export function getData(state) {
     return state.data;
 }
 
+export function getDataSell(state) {
+    return state.dataSell;
+}
+
 export function getLoadData(state) {
     return state.loadData;
 }
